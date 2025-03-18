1428591a3eedec97b47748aefa1a37284f3f4c95
DEC-48 <tanwajava@gmail.com>
2025-03-18 16:50:49 +0700

    feat : getAllworkshop api

--- app/api/workshops/route.ts
+++ app/api/workshops/route.ts
@@ -1,2 +1,30 @@
+import { BuildingParamsSchema } from "@/common/schemas/query";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { string } from "zod";
+
 // Get All Workshops
-export async function GET(request: Request) {}
+export async function GET(request: Request) {
+  const searchParams = new URL(request.url).searchParams;
+  const parameters: string[] = searchParams.get("data")?.split(',') || [];
+  
+  if (parameters.length == 0){ // No parameters
+    const workshops = await prisma.workshop.findMany({
+      include:{
+        workshopSlots:true,
+        intaniaLocation:true
+      }
+    })
+    return NextResponse.json(workshops, {status:200});
+  } else {
+    const selectedFields = BuildingParamsSchema.safeParse(Object.fromEntries(parameters.map(key => [key, true])));
+    if (selectedFields.success){ // valid input
+      const workshops = await prisma.workshop.findMany({
+        select:selectedFields.data
+      })
+      return NextResponse.json(workshops, {status:200});
+    } else { // invalid input
+      return NextResponse.json("invalid query parameter", {status:400});
+    }
+  }
+}
--- common/schemas/query.ts
+++ common/schemas/query.ts
@@ -5,5 +5,12 @@ export const SortingSchema = z.object({
   columns: z.array(z.string().min(1, "Cannot be empty")),
   ascending: z.boolean(),
 });
+export const BuildingParamsSchema = z.object({
+  id: z.boolean().optional(),
+  name: z.boolean().optional(),
+  workshopSlots: z.boolean().optional(),
+  intaniaLocation: z.boolean().optional(),
+  intaniaLocationId: z.boolean().optional()
+}).strict()
 export const GroupingSchema = {};
 export const FetchLevelSchema = {};
