352af8d7552a84825c6af6b7cad594eddfd73902
DEC-48 <tanwajava@gmail.com>
2025-03-18 14:25:42 +0700

    feat : getBuildingById Api

--- app/api/buildings/[id]/route.ts
+++ app/api/buildings/[id]/route.ts
@@ -1,2 +1,26 @@
 // Get Building
-export async function GET(request: Request) {}
+import { prisma } from "@/lib/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { NextResponse } from "next/server";
+
+// Get Building
+export async function GET(request: Request,
+   { params }: { params: Promise<{ id: string }> }) {
+    const {id} = await params
+		let building;
+    try{
+        building = await prisma.building.findFirstOrThrow({
+            where:{
+                id:id
+            },
+        })
+        return NextResponse.json(building, {status:200});
+    } catch (error) {
+			if (error instanceof PrismaClientKnownRequestError){
+				if (error.code === "P2025"){
+					return NextResponse.json("not found",{status:404});
+				}
+			}
+			return NextResponse.json(error, {status:500});
+		}
+}
